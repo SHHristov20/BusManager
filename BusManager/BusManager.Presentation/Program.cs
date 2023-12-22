bbfcd0b54bf4b0e248a99bd21d6a7a0859fccc39
SHHristov20 <SHHristov20@codingburgas.bg>
2023-12-22 21:17:40 +0200

    Create Window Manager to manage app scenes

--- BusManager/BusManager.Presentation/Program.cs
+++ BusManager/BusManager.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using BusManager.Presentation.Views;
+
 namespace BusManager.Presentation
 {
     internal static class Program
@@ -11,7 +13,8 @@ namespace BusManager.Presentation
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginWindow());
+            WindowManager windowManager = WindowManager.Instance;
+            Application.Run(windowManager);
         }
     }
 }
---
+++ BusManager/BusManager.Presentation/Views/WindowManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusManager.Presentation.Views
+{
+    public partial class WindowManager : Form
+    {
+        private static WindowManager instance;
+
+        private readonly LoginWindow loginWindow = new();
+        private readonly RegisterWindow registerWindow = new();
+
+        private WindowManager()
+        {
+            InitializeComponent();
+            LoadScene(SCENES.LOGIN);
+        }
+
+        public static WindowManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WindowManager();
+                }
+                return instance;
+            }
+        }
+
+        public enum SCENES
+        {
+            LOGIN,
+            REGISTER
+        }
+
+        public void LoadScene(SCENES scene)
+        {
+            this.Controls.Clear();
+            switch (scene)
+            {
+                case SCENES.LOGIN:
+                    this.Controls.Add(loginWindow.GetPanel());
+                    break;
+                case SCENES.REGISTER:
+                    this.Controls.Add(registerWindow.GetPanel());
+                    break;
+            }
+        }
+    }
+}
