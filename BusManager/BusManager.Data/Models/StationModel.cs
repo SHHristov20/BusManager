7235035e081684942870bef6ef46967ddc13ba08
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-08 22:04:13 +0200

    Cretae station model

---
+++ BusManager/BusManager.Data/Models/StationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Data.Models
+{
+    public class Station
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public int CityId { get; set; }
+        public City City { get; set; } = null!;
+    }
+}
