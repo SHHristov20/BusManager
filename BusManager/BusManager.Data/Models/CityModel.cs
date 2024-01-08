4a98773c497c94f5480f00d3abf5683faae7ede3
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-08 22:03:54 +0200

    Create city model

---
+++ BusManager/BusManager.Data/Models/CityModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Data.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public ICollection<Station> Stations { get; set; } = null!;
+
+    }
+}
