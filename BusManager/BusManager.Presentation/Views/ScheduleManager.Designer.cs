f37703721580ced9bf7dc09d60a60544a878d039
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-11 15:20:15 +0200

    Create base schedule manager window

---
+++ BusManager/BusManager.Presentation/Views/ScheduleManager.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace BusManager.Presentation.Views
+{
+    partial class ScheduleManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 450);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 30);
+            panel2.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(274, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 25);
+            button3.TabIndex = 2;
+            button3.Text = "Manage Accounts";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(143, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 25);
+            button2.TabIndex = 1;
+            button2.Text = "Manage Stations";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Control;
+            button1.Location = new Point(12, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 25);
+            button1.TabIndex = 0;
+            button1.Text = "Manage Schedules";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // ScheduleManager
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "ScheduleManager";
+            Text = "ScheduleCreator";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+    }
+}
