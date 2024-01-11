c3a034ef07ae8bafb31874058b90b009d8f71ab9
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-11 23:50:18 +0200

    Add ticket repository

---
+++ BusManager/BusManager.Data/Data/Repositories/TicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusManager.Data.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Data.Data.Repositories
+{
+    public class TicketRepository
+    {
+        private readonly BusManagerDbContext _dbContext;
+        public TicketRepository(BusManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
