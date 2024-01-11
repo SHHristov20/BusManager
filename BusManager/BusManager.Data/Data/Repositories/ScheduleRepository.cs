65200674a6c46dca88ac347da786095c2115b81f
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-11 23:49:58 +0200

    Add schedule repository

---
+++ BusManager/BusManager.Data/Data/Repositories/ScheduleRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusManager.Data.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Data.Data.Repositories
+{
+    public class ScheduleRepository
+    {
+        private readonly BusManagerDbContext _dbContext;
+        public ScheduleRepository(BusManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
