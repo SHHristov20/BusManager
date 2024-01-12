64c16f100f35e29275002260dc1405ae3df1be70
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-12 14:28:11 +0200

    Implement ticket service

---
+++ BusManager/BusManager.Core/Implementations/TicketService.cs
@@ -0,0 +1,13 @@
+﻿using BusManager.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Core.Implementations
+{
+    public class TicketService : ITicketService
+    {
+    }
+}
