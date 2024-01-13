b7cc0176468d4aac160b7a9dc05d143d6d5f5abd
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-13 21:33:16 +0200

    Implement code generator

---
+++ BusManager/BusManager.Core/Helpers/CodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusManager.Core.Helpers
+{
+    public class CodeGenerator
+    {
+        private static readonly Random random = new();
+        public static string RandomString(int length)
+        {
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
