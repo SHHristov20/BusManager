ab94ea90a2de9c311d4e3ca741b0696e4bcbfadb
SHHristov20 <SHHristov20@codingburgas.bg>
2024-01-14 20:50:15 +0200

    Change window title

--- BusManager/BusManager.Core/Validators/PasswordValidator.cs
+++ BusManager/BusManager.Core/Validators/PasswordValidator.cs
@@ -4,13 +4,11 @@
     {
         public static string HashPassword(string password)
         {
-            // Generate a salt and hash the password
             return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
         public static bool VerifyPassword(string password, string hashedPassword)
         {
-            // Verify the password against the hashed password
             return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
         }
     }
